33fe89a18f51a9d20369187a3e52a47c8b7f3f1d
ByronB <barquechak@gmail.com>
2024-11-28 01:59:17 -0600

    Updated the DigitalClock for update more efficiently, also changed the transitions for a smooth looking

--- src/components/DigitalClockContainer.tsx
+++ src/components/DigitalClockContainer.tsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from "react";
-import "../styles/digitalClock.css";
+import "../styles/DigitalClockContainer.css";
 
 const DigitalClockContainer: React.FC = () => {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTime(new Date());
+      const currentTime = new Date();
+      // Only update the state if the seconds or minutes change
+      if (currentTime.getSeconds() !== time.getSeconds()) {
+        setTime(currentTime);
+      }
     }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+
+    return () => clearInterval(interval); // Cleanup interval on unmount
+  }, [time]); // Add `time` as a dependency to check only the second change
 
   return (
     <>
